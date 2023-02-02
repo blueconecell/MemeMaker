dfa5dd9b5aa3602f6e2434eda07e9f97ee5cb848
blueconecell <95961799+blueconecell@users.noreply.github.com>
2023-02-02 16:52:47 +0900

    1.5

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.fillRect(200,200,50,200);
-ctx.fillRect(400,200,50,200);
-ctx.lineWidth=2;
-ctx.strokeRect(300, 300, 50, 100);
-ctx.fillRect(200,200,200,20)
-ctx.moveTo(200,200);
-ctx.lineTo(325,100);
-ctx.lineTo(450,200);
+ctx.fillRect(200 + 30,200,20,100)
+ctx.fillRect(400 - 30,200,20,100)
+ctx.fillRect(280,200,60,200)
+
+ctx.arc(310,150,30,0,2*Math.PI)
 ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = "skyblue";
+ctx.arc(300,140,5,0,2*Math.PI)
+ctx.arc(320,140,5,1*Math.PI,2*Math.PI)
+ctx.fill()
