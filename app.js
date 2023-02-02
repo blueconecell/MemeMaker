e2ab11472be499eb0f45ddd47675ddd9978e9d48
blueconecell <95961799+blueconecell@users.noreply.github.com>
2023-02-02 16:32:22 +0900

    1.3

--- app.js
+++ app.js
@@ -3,14 +3,21 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.rect(50, 50, 100, 100);
+ctx.moveTo(50,50);
+ctx.lineTo(150,50);
+ctx.lineTo(150,150);
+ctx.lineTo(50,150);
+ctx.lineTo(50,50);
 ctx.fill();
 
-//분리시키기
-ctx.beginPath()
+// ctx.rect(50, 50, 100, 100);
+// ctx.fill();
 
-//새로운 색 단독적용
-ctx.rect(250, 250, 100, 100);
-ctx.rect(450, 250, 100, 100);
-ctx.fillStyle = "red";
-ctx.fill();
+// //분리시키기
+// ctx.beginPath()
+
+// //새로운 색 단독적용
+// ctx.rect(250, 250, 100, 100);
+// ctx.rect(450, 250, 100, 100);
+// ctx.fillStyle = "red";
+// ctx.fill();
